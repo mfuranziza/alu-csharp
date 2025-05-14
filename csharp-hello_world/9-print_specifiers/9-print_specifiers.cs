fbc96c483cd7b3687abe1257970b3d8cc511399b
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 10:11:07 +0200

    Fix

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,16 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using System.Globalization;
-
 class Program
 {
     static void Main(string[] args)
     {
         double percent = 75.53;
         double money = 98765.4321;
-        
         CultureInfo.CurrentCulture = new CultureInfo("en-US");
-        Console.WriteLine("Percent: {0:0.00}%", percent);
-        Console.WriteLine("Currency: {0:C}", money);
+        Console.WriteLine("Percent: {0:0.00}%\nCurrency: {1:C}", percent, money);
     }
 }
