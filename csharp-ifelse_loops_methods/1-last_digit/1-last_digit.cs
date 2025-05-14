a1cfd414bc75688e4dfd14692c3f052d52c0509c
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 17:34:03 +0200

    Fix: Alphabet game

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        
+        int lastDigit = number % 10;
+        
+        Console.Write("The last digit of " + number + " is " + lastDigit);
+        
+        if (lastDigit > 5)
+        {
+            Console.WriteLine(" and is greater than 5");
+        }
+        else if (lastDigit == 0)
+        {
+            Console.WriteLine(" and is 0");
+        }
+        else
+        {
+            Console.WriteLine(" and is less than 6 and not 0");
+        }
+    }
+}
