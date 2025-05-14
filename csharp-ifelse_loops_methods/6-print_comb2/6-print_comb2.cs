bcb944ff7739aa54b95c53bda63a2984d57dfbfc
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 10:51:33 +0200

    Feat: 6 print

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                result.Append(i.ToString());
+                result.Append(j.ToString());
+
+                if (!(i == 8 && j == 9))
+                {
+                    result.Append(", ");
+                }
+            }
+        }
+
+        Console.WriteLine(result.ToString());
+    }
+}
