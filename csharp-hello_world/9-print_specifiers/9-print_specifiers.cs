8be78f7567482b3ada468e5959830c27f9cdfd5d
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 17:28:50 +0200

    Fix: Print 9 specifiers

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -3,9 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        double percent = 75.53;
+        double percent = .7553;
         double currency = 98765.4321;
-        CultureInfo.CurrentCulture = new CultureInfo("en-US");
-        Console.WriteLine("Percent: {0:0.00}%\nCurrency: {1:C}", percent, money);
+        Console.WriteLine("Percent: {0:P2}", percent);
+        Console.WriteLine("Currency: {0:C}", currency);
     }
 }
