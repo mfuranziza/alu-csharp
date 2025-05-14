711cba6077b15abdf9ed2e75bb3be82a396c86b7
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 10:34:29 +0200

    Feat: 3 print

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                if (c != 'q' && c != 'e')
+                    output += c;
+            }
+            
+            Console.Write(output);
+        }
+    }
+}
