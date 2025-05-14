329dbf4f6d711616a6cb630fd527bcbb2d3d215f
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 09:55:02 +0200

    Fix: 6 print string

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string str = "Holberton School";
-Console.WriteLine($"{str}{str}{str}\n{str.Substring(0, 9)}");
+string result = str + str + str;
+Console.WriteLine(result);
+Console.WriteLine(str.Substring(0, 9));
