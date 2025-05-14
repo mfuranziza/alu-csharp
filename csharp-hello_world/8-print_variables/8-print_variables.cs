4d593daf1051196f5faa05afb4d6dd2e62822e01
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 09:58:06 +0200

    Fix: 8 print string

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int number = 972;
-string street = "Mission Street";
+string street = "Battery Street";
+int number = 98;
 Console.WriteLine("{0} {1}", number, street);
