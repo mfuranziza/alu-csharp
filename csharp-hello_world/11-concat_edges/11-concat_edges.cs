f6e9e8bd26f03980a764ff4c1f67d27f596fb14d
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 11:49:30 +0200

    Feat: Concat Edges

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string s = "C# is an elegant and type-safe object-oriented programming language that enables developers to build a variety of secure and robust applications that run on the .NET Framework.";
-        Console.WriteLine(s.Substring(50, 27) + " in " + s.Substring(0, 2));
+        string str = "C# is an elegant and type-safe object-oriented programming language that enables developers to build a variety of secure and robust applications that run on the .NET Framework.";
+        Console.WriteLine(str.Substring(50, 27) + " in " + str.Substring(0, 2));
     }
 }
