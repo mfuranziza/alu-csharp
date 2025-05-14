1198ad07fd866dd166286e3f0eb374a0a9fa983b
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 10:36:33 +0200

    Feat: 4 hexa

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            
+            for (int i = 0; i <= 98; i++)
+            {
+                output += $"{i} = 0x{i:x}\n";
+            }
+            
+            Console.Write(output);
+        }
+    }
+}
