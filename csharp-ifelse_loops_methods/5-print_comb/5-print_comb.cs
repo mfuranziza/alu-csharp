d69e251fe5aa57a05e58899dd3e1700e3a6526df
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 10:45:55 +0200

    Feat: 0 positive

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string result = "";
+        for (int i = 0; i < 100; i++)
+        {
+            result += i.ToString("D2");
+            if (i != 99)
+                result += ", ";
+        }
+        Console.WriteLine(result);
+    }
+}
