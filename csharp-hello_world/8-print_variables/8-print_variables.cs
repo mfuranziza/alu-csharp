8b226d7204ea709d95468b943041006ad4de227e
Aimelyse Moss <amosekata@gmail.com>
2025-05-14 10:24:09 +0200

    Fix: Program

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -3,8 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        string str1 = "Mission Street";
+        string str1 = "Mission";
+        string str2 = "Street"
         int int1 = 972;
-        Console.WriteLine("{0} {1}", int1, str1);
+        Console.WriteLine("{0} {1} {2}", int1, str1, str2);
     }
 }
