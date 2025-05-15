88d5a6f75a85d25d9c520c960323404acd14e0fa
Aimelyse Moss <amosekata@gmail.com>
2025-05-15 16:29:56 +0200

    Feat: 7 is lower

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -2,23 +2,22 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Random rand = new Random();
-        int number = rand.Next(-10, 11);
-
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        
         if (number > 0)
         {
-            Console.WriteLine($"{number} is positive");
+            Console.WriteLine("{0} is positive", number);
         }
         else if (number == 0)
         {
-            Console.WriteLine($"{number} is zero");
+            Console.WriteLine("{0} is zero", number);
         }
         else
         {
-            Console.WriteLine($"{number} is negative");
+            Console.WriteLine("{0} is negative", number);
         }
     }
 }
-
---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        // ASCII values for lowercase letters are in the range 97-122
+        return c >= 'a' && c <= 'z';
+    }
+}
