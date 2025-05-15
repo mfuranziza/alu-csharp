65a6c05b9dda9e4bfc71e13d0af4d7106a2db5c3
Aimelyse Moss <amosekata@gmail.com>
2025-05-15 16:31:54 +0200

    Feat: 8 last digit

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -4,7 +4,6 @@ class Character
 {
     public static bool IsLower(char c)
     {
-        // ASCII values for lowercase letters are in the range 97-122
         return c >= 'a' && c <= 'z';
     }
 }
---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        
+        Console.Write(lastDigit);
+        
+        return lastDigit;
+    }
+}
