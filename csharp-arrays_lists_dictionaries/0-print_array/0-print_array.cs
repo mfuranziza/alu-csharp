9c523f98061c51a219b0852d89d00de79de5e452
Aimelyse Moss <amosekata@gmail.com>
2025-05-21 10:23:21 +0200

    Feat: 0-print_array

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        
+        int[] newArray = new int[size];
+        
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+        }
+        
+        if (size > 0)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(newArray[i]);
+                if (i < size - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+        
+        return newArray;
+    }
+}
