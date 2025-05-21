c8aa235a8cdfa8a9cb54b445872010ce0c314b90
Aimelyse Moss <amosekata@gmail.com>
2025-05-21 11:13:33 +0200

    Feat: 3 print array

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,18 +4,31 @@
 
 class Array
 {
+
+    static void Main(string[] args)
+    {
+        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array2 = null;
+        int[] array3 = {};
+        int[] array4 = {98, -10, 0, 972, -42};
+
+        Array.Reverse(array1);
+        Array.Reverse(array2);
+        Array.Reverse(array3);
+        Array.Reverse(array4);
+    }
     
     public static void Reverse(int[] array)
     {
         
-        if (array == null || array.Length == 0)
-        {
-            Console.WriteLine();
-            return;
-        }
 
         for (int i = array.Length - 1; i >= 0; i--)
         {
+            if (array == null || array.Length == 0)
+        {
+            Console.WriteLine(array[i]);
+            return;
+        }
             Console.Write(array[i]);
             if (i > 0)
                 Console.Write(" ");
