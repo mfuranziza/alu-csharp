b7b6b4af292d675a32fe078df2396b1dbf1328ef
Aimelyse Moss <amosekata@gmail.com>
2025-05-28 09:53:29 +0200

    4 print

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            result.Add(i);
+        }
+
+        for (int i = 0; i < result.Count; i++)
+        {
+            Console.Write(result[i]);
+            
+            if (i < result.Count - 1)
+                Console.Write(" ");
+        }
+        
+        Console.WriteLine();
+
+        return result;
+    }
+}
