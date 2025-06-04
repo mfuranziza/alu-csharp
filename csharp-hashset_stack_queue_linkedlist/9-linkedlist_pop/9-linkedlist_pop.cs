0dfcf46c982b1222468707efa1b3ce6d35bb8105
Aimelyse Moss <amosekata@gmail.com>
2025-06-04 02:39:09 +0200

    Feat: 10 Linked list

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -3,19 +3,6 @@ using System.Collections.Generic;
 
 public class LList
 {
-    static void Main(string[] args)
-    {
-        LinkedList<int> myLList = new LinkedList<int>();
-
-        myLList.AddLast(11);
-        myLList.AddLast(3);
-        myLList.AddLast(-9);
-        myLList.AddLast(47);
-        myLList.AddLast(0);
-        myLList.AddLast(-9);
-
-        Console.WriteLine(LList.Pop(myLList));
-    }
 
     public static LinkedList<int> CreatePrint(int size)
     {
