fd9a47a9031b95d5e2ef29a93f7c702f9332b6b1
Aimelyse Moss <amosekata@gmail.com>
2025-06-04 02:42:39 +0200

    Feat: 11 Linked list

--- csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        
+        if (size < 0)
+        {
+            return list;
+        }
+        
+        for (int i = 0; i < size; i++)
+        {
+            list.AddLast(i);
+            Console.WriteLine(i);
+        }
+        
+        return list;
+    }
+    
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        
+        foreach (int item in myLList)
+        {
+            count++;
+        }
+        
+        return count;
+    }
+    
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        return myLList.AddFirst(n);
+    }
+    
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return index;
+            }
+            index++;
+        }
+        
+        return -1;
+    }
+    
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList == null || myLList.Count == 0 || myLList.First == null)
+        {
+            return 0;
+        }
+        
+        int value = myLList.First.Value;
+        myLList.RemoveFirst();
+        return value;
+    }
+    
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList == null || n < 0)
+        {
+            return 0;
+        }
+        
+        int index = 0;
+        
+        foreach (int item in myLList)
+        {
+            if (index == n)
+            {
+                return item;
+            }
+            index++;
+        }
+        
+        return 0;
+    }
+    
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        
+        foreach (int item in myLList)
+        {
+            sum += item;
+        }
+        
+        return sum;
+    }
+}
