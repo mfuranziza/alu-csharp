f702e59d2bbc41dd3f97da4ef85184168b9bef0d
Aimelyse Moss <amosekata@gmail.com>
2025-06-04 02:25:53 +0200

    Feat: 7 Linked list

--- csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        
+        if (size < 0)
+        {
+            return list;
+        }
+        
+        for (int i = 0; i < size; i++)
+        {
+            list.AddLast(i);
+            Console.WriteLine(i);
+        }
+        
+        return list;
+    }
+    
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        
+        foreach (int item in myLList)
+        {
+            count++;
+        }
+        
+        return count;
+    }
+    
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        return myLList.AddFirst(n);
+    }
+}
