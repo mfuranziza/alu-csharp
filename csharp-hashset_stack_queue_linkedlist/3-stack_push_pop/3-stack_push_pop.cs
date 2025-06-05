695a17534fd801bf12dd483398942bf7338829fc
Aimelyse Moss <amosekata@gmail.com>
2025-06-05 18:55:42 +0200

    Fix: 3 push pop

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,63 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class MyStack
+public static class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Number of items: " + aStack.Count);
-        
+        string item = "";
+
+        Console.WriteLine("Number of items: {0}", aStack.Count);
         if (aStack.Count == 0)
         {
-            Console.WriteLine("Stack is empty");
+            item = "Stack is empty";
         }
         else
         {
-            Console.WriteLine("Top item: " + aStack.Peek());
+            item = "Top item: " + aStack.Peek();
         }
-        
-        bool contains = false;
-        foreach (string item in aStack)
+        Console.WriteLine(item);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+        while (aStack.Contains(search))
         {
-            if (item == search)
-            {
-                contains = true;
-                break;
-            }
+            aStack.Pop();
         }
-        
-        Console.WriteLine("Stack contains \"" + search + "\": " + contains);
-        
-        if (contains)
-        {
-            Stack<string> tempStack = new Stack<string>();
-            string[] items = new string[aStack.Count];
-            aStack.CopyTo(items, 0);
-            
-            aStack.Clear();
-            
-            bool foundSearch = false;
-            for (int i = 0; i < items.Length; i++)
-            {
-                if (items[i] == search)
-                {
-                    foundSearch = true;
-                    break;
-                }
-                if (!foundSearch)
-                {
-                    tempStack.Push(items[i]);
-                }
-            }
-            
-            while (tempStack.Count > 0)
-            {
-                aStack.Push(tempStack.Pop());
-            }
-        }
-        
         aStack.Push(newItem);
-        
+
         return aStack;
     }
 }
