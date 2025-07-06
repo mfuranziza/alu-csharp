c38a32862d86c55074f3182503b0b7a8d0b3e9c3
Aimelyse Moss <amosekata@gmail.com>
2025-07-06 22:13:44 +0200

    Fix: 3 Type Get

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#nullable disable
+using System;
 using System.Reflection;
 
 /// <summary>
@@ -13,13 +14,13 @@ public class Obj
     public static void Print(object myObj)
     {
         Type type = myObj.GetType();
-
+        
         Console.WriteLine($"{type.Name} Properties:");
         foreach (PropertyInfo prop in type.GetProperties())
         {
             Console.WriteLine(prop.Name);
         }
-
+        
         Console.WriteLine($"{type.Name} Methods:");
         foreach (MethodInfo method in type.GetMethods())
         {
